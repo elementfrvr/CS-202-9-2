d995430638bf0860ce9d733bb92e102820a8079d
elementfrvr <dakota.mitchem@siu.edu>
2015-12-18 01:28:53 -0600

    Create Textbook.java

---
+++ Textbook.java
@@ -0,0 +1,11 @@
+
+public class Textbook extends Book {
+	//Constructor
+	public Textbook(String title, String author, int pages) {
+		super(title, author, pages);
+	}
+	public void purchase(){
+		System.out.println("Your total comes out to 1 arm and 1 leg");
+	}
+
+}
