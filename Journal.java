6091558c27ceca2f1eac254d4a73fbc780077fc1
elementfrvr <dakota.mitchem@siu.edu>
2015-12-18 01:27:33 -0600

    Create Journal.java

---
+++ Journal.java
@@ -0,0 +1,11 @@
+
+public class Journal extends Book {
+	//Constructor
+	public Journal(String title, String author, int pages) {
+		super(title, author, pages);
+	}
+	public void article(){
+		System.out.println("E= MC^2");
+	}
+
+}
