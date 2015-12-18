1e9bdf35cdae1004c2cba2afba2c6575d4c122ee
elementfrvr <dakota.mitchem@siu.edu>
2015-12-18 01:27:56 -0600

    Create Magazine.java

---
+++ Magazine.java
@@ -0,0 +1,11 @@
+
+public class Magazine extends Book {
+	//Constructor
+	public Magazine(String title, String author, int pages) {
+		super(title, author, pages);
+	}
+	public void gossip(){
+		System.out.println("Paul is Dead");
+	}
+
+}
